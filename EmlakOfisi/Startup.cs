add07d257963af4c3af441282a63b1e472330e1f
Mehmet Kemal Yıldırım <mehmet.kemal111@gmail.com>
2021-05-05 16:26:26 +0300

    Add project files.

---
+++ EmlakOfisi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmlakOfisi.Startup))]
+namespace EmlakOfisi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
